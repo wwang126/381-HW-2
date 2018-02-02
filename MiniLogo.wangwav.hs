2603d87f323a04286c04b623bd7900af1f23489e
wwang126 <wavelet.wang@gmail.com>
2018-02-01 18:27:07 -0800

    added some definitions and basic comment framework

---
+++ MiniLogo.wangwav.hs
@@ -0,0 +1,26 @@
+module MiniLogo where
+
+--Wavelet Wang
+--wangwav
+--HW2
+--Feb-1-18
+
+--Define Basic MiniLogo syntax
+
+--Define a program as set of commands
+Prog = [Cmd]
+
+--Define the pen mode
+data Mode = Up
+        | Down
+        deriving(Show,Eq)
+
+-- Define Line Macro
+
+-- Define Nix Macro
+
+-- Define Steps Macro
+
+-- Define macros function
+
+-- Define pretty function
